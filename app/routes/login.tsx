bd3918b83a26c3ca0dbc6c289bb76cbf5b194383
Avery <trajano.juan@gmail.com>
2022-05-25 11:23:44 -0300

    feat: :sparkles: Set up a login route
    set up a /login route where sign in and sign up forms will live.

---
+++ app/routes/login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h2 className="text-yellow-300 font-extrabold text-5xl">Login Route</h2>
+    </div>
+  );
+}
