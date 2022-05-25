1e88c8d72c6795bb0d12b073aa84e3901198b74b
Avery <trajano.juan@gmail.com>
2022-05-25 12:07:58 -0300

    feat: :sparkles: Create the sign in form
    Build the form; Create a form field component

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { Layout } from "~/components/layout";
+import { FormField } from "~/components/form-field";
+
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Updates the form data when an input changes
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData((form) => ({ ...form, [field]: event.target.value }));
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h2 className="text-yellow-300 font-extrabold text-5xl">Login Route</h2>
-    </div>
+    <Layout>
+      <div className="h-full justify-center items-center flex flex-col gap-y-4">
+        <h2 className="text-5xl font-extrabold text-yellow-300">
+          Welcome to Kudos!
+        </h2>
+        <p className="font-semibold text-slate-300">
+          Log In To Give Some Praise!
+        </p>
+
+        <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
+          <FormField
+            htmlFor="email"
+            label="Email"
+            value={formData.email}
+            onChange={(e) => handleInputChange(e, "email")}
+          />
+          <FormField
+            htmlFor="password"
+            type="password"
+            label="Password"
+            value={formData.password}
+            onChange={(e) => handleInputChange(e, "password")}
+          />
+          <div className="w-full text-center">
+            <input
+              type="submit"
+              className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+              value="Sign In"
+            />
+          </div>
+        </form>
+      </div>
+    </Layout>
   );
 }
