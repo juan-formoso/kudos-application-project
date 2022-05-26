b83a64d1238f015fe45815b20696ece332950b1e
Avery <trajano.juan@gmail.com>
2022-05-26 12:52:53 -0300

    feat: :sparkles: Add a sign up form
    Store the form action in state; Add toggleable fields

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,9 +3,12 @@ import { Layout } from "~/components/layout";
 import { FormField } from "~/components/form-field";
 
 export default function Login() {
+  const [action, setAction] = useState("login");
   const [formData, setFormData] = useState({
     email: "",
     password: "",
+    firstName: "",
+    lastName: "",
   });
 
   // Updates the form data when an input changes
@@ -19,11 +22,20 @@ export default function Login() {
   return (
     <Layout>
       <div className="h-full justify-center items-center flex flex-col gap-y-4">
+        <button
+          onClick={() => setAction(action == "login" ? "register" : "login")}
+          className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+        >
+          {action === "login" ? "Sign Up" : "Sign In"}
+        </button>
+
         <h2 className="text-5xl font-extrabold text-yellow-300">
           Welcome to Kudos!
         </h2>
         <p className="font-semibold text-slate-300">
-          Log In To Give Some Praise!
+          {action === "login"
+            ? "Log In To Give Some Praise!"
+            : "Sign Up To Get Started!"}
         </p>
 
         <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
@@ -40,12 +52,31 @@ export default function Login() {
             value={formData.password}
             onChange={(e) => handleInputChange(e, "password")}
           />
+          {action === "register" && (
+            <>
+              <FormField
+                htmlFor="firstName"
+                label="First Name"
+                onChange={(e) => handleInputChange(e, "firstName")}
+                value={formData.firstName}
+              />
+              <FormField
+                htmlFor="lastName"
+                label="Last Name"
+                onChange={(e) => handleInputChange(e, "lastName")}
+                value={formData.lastName}
+              />
+            </>
+          )}
           <div className="w-full text-center">
-            <input
+            <button
               type="submit"
+              name="_action"
+              value={action}
               className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
-              value="Sign In"
-            />
+            >
+              {action === "login" ? "Sign In" : "Sign Up"}
+            </button>
           </div>
         </form>
       </div>
