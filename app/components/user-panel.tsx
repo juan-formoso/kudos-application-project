e8a58628aad6b405997887648ebd26555b8308cb
Avery <trajano.juan@gmail.com>
2022-06-03 12:04:14 -0300

    feat: :sparkles: Add the user list panel.
    Add user-panel.tsx in app/components; Update home.tsx in app/routes

---
+++ app/components/user-panel.tsx
@@ -0,0 +1,22 @@
+// This creates the side panel that will contain the list of users. The component is static though, which means it does not perform any actions or vary in any way.
+
+export function UserPanel() {
+  return (
+    <div className="w-1/6 bg-gray-200 flex flex-col">
+      <div className="text-center bg-gray-300 h-20 flex items-center justify-center">
+        <h2 className="text-xl text-blue-600 font-semibold">My Team</h2>
+      </div>
+      <div className="flex-1 overflow-y-scroll py-4 flex flex-col gap-y-10">
+        <p>Users go here!</p>
+      </div>
+      <div className="text-center p-6 bg-gray-300">
+        <button
+          type="submit"
+          className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,21 @@
+// This code imports the UserPanel and the Layout components, then renders the UserPanel within the layout.
+
 import type { LoaderFunction } from "@remix-run/node";
 import { requireUserId } from "~/utils/auth.server";
+import { Layout } from "~/components/layout";
+import { UserPanel } from "~/components/user-panel";
 
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUserId(request);
-  return null;
+  return null; // A loader always has to return some value, even if that is null.
 };
 
 export default function Home() {
-  return <h2>Home Page</h2>;
+  return (
+    <Layout>
+      <div className="h-full flex">
+        <UserPanel />
+      </div>
+    </Layout>
+  );
 }
