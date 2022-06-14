cc2457974820c8d052dd935d738ffb3842e15b02
Avery <trajano.juan@gmail.com>
2022-06-13 21:30:59 -0300

    feat: :sparkles: Add the logout function.
    Update user-panel.tsx in app/components; Update logout.ts in app/routes

--- app/components/user-panel.tsx
+++ app/components/user-panel.tsx
@@ -18,12 +18,14 @@ export function UserPanel({ users }: { users: User[] }) {
         ))}
       </div>
       <div className="text-center p-6 bg-gray-300">
-        <button
-          type="submit"
-          className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
-        >
-          Sign Out
-        </button>
+        <form action="/logout" method="post">
+          <button
+            type="submit"
+            className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+          >
+            Sign Out
+          </button>
+        </form>
       </div>
     </div>
   );
